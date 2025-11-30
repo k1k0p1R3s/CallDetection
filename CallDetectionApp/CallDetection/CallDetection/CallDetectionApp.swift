608332325f89cf0c2c13afd97948a2029ce0c50c
Francisco Pires <fpires@Franciscos-MBP.(none)>
2025-11-30 01:33:33 +0000

    Initial project structure

---
+++ CallDetectionApp/CallDetection/CallDetection/CallDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallDetectionApp.swift
+//  CallDetection
+//
+//  Created by Francisco Pires on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CallDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
